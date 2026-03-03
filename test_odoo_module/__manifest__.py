b3a5dc11a831806419c5e271bfaab9cc4c19d135
nada-adel-mohamed <nada.adel@lumerai-llc.com>
2026-03-03 16:05:43 +0200

    feat: Add manifest file for the new "Test Odoo Module".

--- test_odoo_module/__manifest__.py
+++ test_odoo_module/__manifest__.py
@@ -3,7 +3,6 @@
     "summary": "A module to test pre-commit hooks",
     "version": "18.0.1.0.0",
     "category": "Extra Tools",
-    "author": "Odoo Community Association (OCA), Test Company",
     "website": "https://github.com/OCA/python-precommit-demo",
     "license": "LGPL-3",
     "depends": ["base"],
