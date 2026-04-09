1bb97cd3bdb02d9c61b1b882d7f0e06d09ad4418
Nada-Adel-Anan <nada.adel@softspace.ae>
2026-04-09 20:06:01 +0200

    test2

--- test_odoo_module/__manifest__.py
+++ test_odoo_module/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Test Odoo Module",
-    "summary": "A module to test pre-commit hooks v44",
+    "summary": "A module to test pre-commit hooks v3",
     "version": "18.0.1.0.0",
     "category": "Extra Tools",
     "website": "https://github.com/OCA/python-precommit-demo",
