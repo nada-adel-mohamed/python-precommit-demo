bce0dfaff682ef5d1e6b1cf1cfdfb18d886c613b
Nada-Adel-Anan <nada.adel@softspace.ae>
2026-04-09 22:41:36 +0200

    testtt bugs

--- test_odoo_module/models/order.py
+++ test_odoo_module/models/order.py
@@ -31,5 +31,6 @@ class TestOrder(models.Model):
     def action_confirm(self):
         """Confirm the order."""
         self.write({"state": "confirmed"})
-        _logger.info("Order %s confirmed", self.name)
+        # BUG: Undefined variable 'undefined_var' - will cause NameError
+        _logger.info("Order %s confirmed by %s", self.name, undefined_var)
         return True
