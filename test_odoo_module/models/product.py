69ecf8af6a6587090a9ba30c61d3bccf4772c873
Nada-Adel-Anan <nada.adel@softspace.ae>
2026-04-09 21:56:38 +0200

    /0

--- test_odoo_module/models/product.py
+++ test_odoo_module/models/product.py
@@ -14,3 +14,10 @@ class TestProduct(models.Model):
     name = fields.Char(string="Product Name", required=True)
     price = fields.Float(string="Price", required=True)
     active = fields.Boolean(default=True)
+
+    def calculate_tax(self):
+        """Calculate tax on price."""
+        # BUG: Division by zero when tax_rate is 0
+        tax_rate = 0
+        tax = self.price / tax_rate
+        return tax
